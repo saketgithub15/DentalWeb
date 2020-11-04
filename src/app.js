945c0c01812f46488637e5e465ff657682166c76
saket <saket@coder.com>
2020-11-04 23:32:03 +0530

    updated footer

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path  = require('path');
 const hbs = require('hbs');
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // console.log(path.join(__dirname, "../public"))
 const staticPath = (path.join(__dirname, "../public"));
